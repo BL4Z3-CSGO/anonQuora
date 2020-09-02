0b69a5a5c9a9e38dae89ddcc6c98130eb4a4d558
felix12340000 <58826968+BL4Z3-CSGO@users.noreply.github.com>
2020-09-02 02:49:22 +0200

    Update anonQuora.js

--- anonQuora.js
+++ anonQuora.js
@@ -5,6 +5,7 @@
 // @author       felix12340000
 // @match        https://www.quora.com/*
 // ==/UserScript==
+
 if (window.location.href.slice(-8) != "?share=1") {
     window.location.replace(window.location.href + "?share=1");
 }
