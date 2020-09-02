76820babc8585154770a6de3b8f2a79eeb7ad589
felix12340000 <58826968+BL4Z3-CSGO@users.noreply.github.com>
2020-09-02 04:14:09 +0200

    Update user.js

--- user.js
+++ user.js
@@ -4,8 +4,9 @@
 // @description  lets you use quora without having to log in
 // @author       felix12340000
 // @match        https://www.quora.com/*
+// @run-at document-start
 // ==/UserScript==
 
-if (window.location.href.slice(-8) != "?share=1") {
+if (window.location.href.slice(-8) != "?share=1" && window.location.href.slice(0 , 31) != "https://www.quora.com/search?q=") {
     window.location.replace(window.location.href + "?share=1");
 }
